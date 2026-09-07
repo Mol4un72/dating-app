d9c7156f095e83e898b721cd9978013b18c78957
Mol4un72 <pomaino12@gmail.com>
2026-09-07 12:24:43 +0300

    feat: update app layout

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,12 +1,20 @@
+import { auth } from '@/auth'
+import { redirect } from 'next/navigation'
 import { AppShell } from '@/components/app-shell'
 import { FiltersProvider } from '@/context/filters-context'
 import { LikesProvider } from '@/context/likes-context'
 
-export default function AppGroupLayout({
+export default async function AppGroupLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await auth()
+
+  if (!session?.user) {
+    redirect('/login')
+  }
+
   return (
     <LikesProvider>
       <FiltersProvider>
